52c13ee4fc71fe72b060f227193a400d2fa6c93a
MinkieYume <sign@yumieko.com>
2025-12-02 12:33:16 +0800

    test(runtime): add runtime tests for evaluation and file loading
    新增了 runtime 相关的测试，包括字符串求值、Guile 环境下的求值、获取数值和字符串结果、加载文件以及定义变量等功能的测试。确保运行时的各项功能正常工作。

---
+++ tests/runtime.rs
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// SPDX-FileCopyrightText: 2024 MinkieYume <minkieyume@yumieko.com>
+use ngrs::*;
+use std::sync::Once;
+use std::env;
+use std::path::PathBuf;
+
+static INIT: Once = Once::new();
+
+#[ctor::ctor]
+fn global_init() {
+    INIT.call_once(|| {
+        Runtime::initialize();
+    });
+}
+
+#[test]
+fn can_eval_string() {
+    let runtime = Runtime::new();
+    runtime.eval_string("(+ 1 2 3)");
+    assert!(true, "Evaluated successfully");
+}
+
+#[test]
+fn can_eval_with_guile() {
+    with_guile(|vm| {
+        vm.eval_string("(+ 1 2 3)");            
+    });
+}
+
+#[test]
+fn can_eval_get_value() {
+    with_guile(|vm| {
+        let value = vm.eval_string("(+ 1 2 3)");
+        println!("Evaluated value: {:?}", value);
+    });
+}
+
+#[test]
+fn can_eval_and_get_number() {
+    with_guile(|vm| {
+        let value:i32 = vm.eval_string("(+ 1 2 3)").try_into().unwrap();
+        assert_eq!(value, 6);
+    });
+}
+
+#[test]
+fn can_eval_and_get_string() {
+    with_guile(|vm| {
+        let value:String = vm.eval_string("(string-append \"kkp\" \"PPL\")").try_into().unwrap();
+        assert_eq!(value, "kkpPPL");
+    });
+}
+
+#[test]
+fn can_load_file() {    
+    with_guile(|vm| {
+        let current_dir:PathBuf = env::current_dir().unwrap();
+        let path = current_dir.join("scm/test-loadfile.scm");
+        let filename = path.to_str().unwrap();
+        let scm = vm.primitive_load(filename);
+        assert_eq!(scm.to_string(), "6");
+    });
+}
+
+#[test]
+fn can_define() {    
+    with_guile(|vm| {
+        vm.define("num", &SCM::from(42_i32));
+        let scm = vm.eval_string("num");
+        let num:i32 = scm.try_into().unwrap();
+        assert_eq!(num, 42_i32);
+    });
+}
